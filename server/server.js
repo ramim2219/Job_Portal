27f4fb309afbe6874c1b06a6cd6bfa8ffe227243
ramim2219 <shafayetullah200119@gmail.com>
2025-11-30 02:34:34 +0600

    backend work started

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+// initialize express
+const app = express()
+
+//Middlewares
+app.use(cors())
+app.use(express.json())
+
+//Routes
+app.get('/',(req,res)=>res.send("API Working"))
+
+// Port 
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
