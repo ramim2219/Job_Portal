467cbb505f2f94348c73eea2f251216978e4e6b4
ramim2219 <shafayetullah200119@gmail.com>
2025-12-01 00:35:23 +0600

    connected database

--- server/server.js
+++ server/server.js
@@ -1,20 +1,23 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/db.js';
 
-// initialize express
-const app = express()
+const app = express();
 
-//Middlewares
-app.use(cors())
-app.use(express.json())
+// Connect DB
+await connectDB();
 
-//Routes
-app.get('/',(req,res)=>res.send("API Working"))
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => res.send("API Working"));
 
 // Port 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
